e9818be0c6048233bf7b3b392c82bfaed1d4aad1
LuWeijun <luweijun@weidian.com>
2016-07-14 20:41:57 +0800

    Initial Commit

---
+++ FNPaperShredder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FNPaperShredder
+//
+//  Created by Fnoz on 16/7/14.
+//  Copyright © 2016年 Fnoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
